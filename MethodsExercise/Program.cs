058b6158c9c666c2ae350ee4bd5918aa33a0f250
Faithtorian Goins <faithtoriangoins@gmail.com>
2023-01-31 15:01:58 -0600

    created a method.

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -4,7 +4,23 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hi there, what is your name?");
+            string userName = Console.ReadLine();
+            Console.WriteLine();
+            Console.WriteLine($"it's nice to meet you {userName}. \n" +
+                              $"the supperbowl is coming up. \n" +
+                              $"what is your favorite gameday snack?");
+            string snackName = Console.ReadLine();
+            Console.WriteLine();
+            Console.WriteLine($"oooo {snackName} those are the best! \n" +
+                              $"what about your favorite drink?...");
+            Console.WriteLine();
+            string drinkName = Console.ReadLine();
+            Console.WriteLine();
+            Console.WriteLine($"{drinkName} and {snackName} sounds great! \n" +
+                              $"i'll be at your house 30 minutes before, \n" +
+                              $"first Down make sure you have the {snackName} \n" +
+                              $"and the {drinkName} ready. See you Game Day!");
         }
     }
 }
